b53e513241e9e768f7d984c3fd1997d0807be604
everfolder <dimamotolin@gmail.com>
2026-06-12 18:39:37 +0300

    добавил роутер + helpers в стили мейби даже работает

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import CatalogueView from "@/views/CatalogueView.vue"
+import CarDetail from "@/views/CataloqueDetail.vue"
+import MainView from "@/views/MainView.vue";
+import NewsView from "@/views/NewsView.vue";
+import ContactsView from "@/views/ContactsView.vue";
+
+
+const routes = [
+    {
+        path: '/',
+        component: MainView
+    },
+    {
+        path: '/catalogue',
+        component: CatalogueView
+    },
+    {
+        path: '/catalogue/:id',
+        component: CarDetail,
+        props: true
+    },
+    {
+        path: '/news',
+        component: NewsView
+    },
+    {
+        path: '/contacts',
+        component: ContactsView
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return { top: 0 }
+    }
+})
+
+export default router
