9e043bcaf84745fa540066a5e8b9253c47e89f5a
luvxtears <polikanovapol@mail.ru>
2026-06-28 10:51:59 +0300

    доделана страница сервиса, но без новостей, тк тогда нужно редачить компоненты новостей а я очень не хочу

--- src/pages/Service.vue
+++ src/pages/Service.vue
@@ -2,13 +2,14 @@
 import GrayForm from '@/components/GrayForm.vue';
 import ServiceText from '@/components/Service/ServiceText.vue';
 import ServiceServices from '@/components/Service/ServiceServices.vue';
+import ServiceContacts from '@/components/Service/ServiceContacts.vue';
 </script>
 
 <template>
     <ServiceText />
     <GrayForm title="Запись на сервис" />
     <ServiceServices />
-    <!--жду крч-->
+    <ServiceContacts title="Сервисные центры"/>
 </template>
 
 <style scoped lang="scss"></style>
